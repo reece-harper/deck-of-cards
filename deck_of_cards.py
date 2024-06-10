b3c3844b797303925a7a951a2d8a3c63451a8831
reece-harper <reece.harper@rbnorwich.org.uk>
2024-06-10 15:14:01 +0100

    Add files via upload

---
+++ deck_of_cards.py
@@ -0,0 +1,25 @@
+﻿from ast import In
+from random import shuffle
+suits = ["♥", "♦", "♣", "♠"]
+deck = []
+
+
+
+for item in suits:
+    for x in range(1, 14):
+        if x == 11:
+            deck.append("J" + item)
+        elif x == 12:
+            deck.append("Q" + item)
+        elif x == 13:
+            deck.append("K" + item)
+        elif x == 14:
+            deck.append("A" + item)
+        else:
+            deck.append(f"{x}" + item)
+
+print(deck)
+input("please press enter to shuffle")
+shuffle(deck)
+print(deck)
+      
